5b08ce94ed774613f447d9880ef9fea892333a64
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-25 23:57:03 +0000

    Add test code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,4 +21,23 @@ $(document).ready(() => {
 
     $tweet.append($header);
   }
+
+  // TESTING
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+
+  const $tweet = createTweetElement(tweetData);
+  
+  console.log($tweet);
+  $('#tweets-container').append($tweet);
 })

8c6304aa8597cb3581e87fff93a949dbac640406
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-25 19:14:26 +0000

    Update the character counter when text is inputted with the remaining character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 // when DOM loads
 $(document).ready(() => {
 
-  // tweet input area
   const $textArea = $('.new-tweet textarea');
 
-  // when text is inputted (keyup)
+  // updates the character counter every time text is inputted
   $textArea.on('keyup', function(e) {
-   console.log(140 - $(this).val().length);
+    
+    const remainingChars = 140 - $(this).val().length;
+
+    $(this).siblings('.counter').text(remainingChars);
   })
 
 })  

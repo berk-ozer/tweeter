1d81f0d3b66e0cf6cce4b3ac32687535cc48c9e1
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-25 23:56:29 +0000

    Create createTweetElement function. Build header element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,24 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// When DOM is loaded
+$(document).ready(() => {
+  
+  // Takes in a tweet object, returns an <article> element containing the tweet
+  const createTweetElement = tweetData => {
+    
+    // The tweet article which will be returned in the end
+    const $tweet = $('<article>').addClass('tweet');
 
+    // Creating the header and appending it to $tweet
+    const $header = $('<header>');
+
+    const $img = $('<img>').attr({
+      src: tweetData.user.avatars,
+      alt: `${tweetData.user.handle}-avatar`
+    });
+
+    $header.append($img);
+    $header.append($('<span>').text(tweetData.user.name));
+    $header.append($('<span>').text(tweetData.user.handle));
+
+    $tweet.append($header);
+  }
+})

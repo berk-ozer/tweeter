9861597252f8e7de967b5d91e6d6b2dc6c3d3b84
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-25 19:02:50 +0000

    Setup textarea selector, keyup event listener

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+// when DOM loads
+$(document).ready(() => {
+
+  // tweet input area
+  const $textArea = $('.new-tweet textarea');
+
+  // when text is inputted (keyup)
+  $textArea.on('keyup', function(e) {
+   console.log($(this).val());
+  })
+
+})  

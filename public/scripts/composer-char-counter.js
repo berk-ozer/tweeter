52adb2c98dd8465440eee9408fd22763779d1d66
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-25 19:43:26 +0000

    Turn character counter text red when user goes over character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,10 +5,19 @@ $(document).ready(() => {
 
   // updates the character counter every time text is inputted
   $textArea.on('keyup', function(e) {
-    
-    const remainingChars = 140 - $(this).val().length;
 
-    $(this).siblings('.counter').text(remainingChars);
+    const $charCounter = $(this).siblings('.counter');
+    const remainingCharCount = 140 - $(this).val().length;
+
+    $charCounter.text(remainingCharCount);
+
+    // makes character counter red if over character limit
+    if (remainingCharCount < 0) {
+      $charCounter.addClass('over-char-limit');
+
+    } else {
+      $charCounter.removeClass('over-char-limit');
+    }
   })
 
 })  

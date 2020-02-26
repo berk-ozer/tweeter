e9f222429378010f9c6124a9732444834361d31b
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-26 16:05:37 +0000

    Move functions expressions outside of the document.ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,42 +1,46 @@
-// When DOM is loaded
+// 
+// FUNCTIONS
+// 
+
+// Takes in a tweet object, returns an <article> element containing the tweet
+const createTweetElement = tweetData => {
+
+  // The tweet article element which will be returned in the end
+  const $tweet = $('<article>').addClass('tweet');
+
+  const daysSinceTweet = (Date.now() - tweetData.created_at) / 86400000;
+
+  // The tweet element's inner html
+  const htmlContent = `
+    <header>
+      <img src=${tweetData.user.avatars} alt="${tweetData.user.handle}-avatar">
+      <span>${tweetData.user.name}</span>
+      <span>${tweetData.user.handle}</span>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer>
+      ${Math.round(daysSinceTweet)} days ago
+      <span>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+    </footer>
+  `
+
+  // Add the inner html to tweet and return it
+  return $tweet.html(htmlContent);
+};
+
+// Takes in an array of tweet objects, turns them into html elements, and appends them to #tweets-container
+const renderTweets = tweetsDatabase => {
+  for (const tweet of tweetsDatabase) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+};
+
+// When DOM is loaded, call the functions
 $(document).ready(() => {
-  
-  // Takes in a tweet object, returns an <article> element containing the tweet
-  const createTweetElement = tweetData => {
-
-    // The tweet article element which will be returned in the end
-    const $tweet = $('<article>').addClass('tweet');
-
-    const daysSinceTweet = (Date.now() - tweetData.created_at) / 86400000;
-
-    // The tweet element's inner html
-    const htmlContent = `
-      <header>
-        <img src=${tweetData.user.avatars} alt="${tweetData.user.handle}-avatar">
-        <span>${tweetData.user.name}</span>
-        <span>${tweetData.user.handle}</span>
-      </header>
-      <p>${tweetData.content.text}</p>
-      <footer>
-        ${Math.round(daysSinceTweet)} days ago
-        <span>
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </span>
-      </footer>
-    `
-
-    // Add the inner html to tweet and return it
-    return $tweet.html(htmlContent);
-  };
-
-  // Takes in an array of tweet objects, turns them into html elements, and appends them to #tweets-container
-  const renderTweets = tweetsDatabase => {
-    for (const tweet of tweetsDatabase) {
-      $('#tweets-container').append(createTweetElement(tweet));
-    }
-  };
 
   // TESTING
   const data = [

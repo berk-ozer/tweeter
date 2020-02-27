6c422b1ac16af24714631f44fb4471e9c482f474
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-27 21:18:27 +0000

    Change show/hide animations to fadeIn/fadeOut

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -7,13 +7,13 @@ $(document).ready(() => {
     // check if we are all the way to the top
     // if yes, show the 'write a new tweet' on navbar, hide scroll up button on bottom right
     if ($(this).scrollTop() === 0) {
-      $('nav span:nth-child(2)').show(200);
-      $('.scroll-up').hide(200)
+      $('nav span:nth-child(2)').fadeIn(200);
+      $('.scroll-up').fadeOut(200)
   
       // if no, do vice versa
     } else {
-      $('nav span:nth-child(2)').hide(200);
-      $('.scroll-up').show(200)
+      $('nav span:nth-child(2)').fadeOut(200);
+      $('.scroll-up').fadeIn(200)
     }
   })
 
